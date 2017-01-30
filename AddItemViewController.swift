07fe9ff7ac5811f07388a5c650580149b60f6c81
José Gutiérrez <guti7@users.noreply.github.com>
2017-01-29 22:16:28 -0800

    Add static table cells to Add Item screen

--- AddItemViewController.swift
+++ AddItemViewController.swift
@@ -11,13 +11,22 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     
+    
     // MARK: - Actions
+    
     @IBAction func cancel() {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func done() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Table View Delegate
+        
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
     
 }
