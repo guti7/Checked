d74d8550dbf4dd54e0ead247817506e259a1df70
José Gutiérrez <guti7@users.noreply.github.com>
2017-01-29 21:04:32 -0800

    Add rows to the table view

--- ChecklistViewController.swift
+++ ChecklistViewController.swift
@@ -48,6 +48,21 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - Actions
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.description = "I am a new row"
+        item.checked = false
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+    
 
     // MARK: - Data Source
     
